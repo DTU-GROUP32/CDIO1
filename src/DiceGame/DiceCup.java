6f14f12c5ea1e386a9675f8353c50fcd5693dba3
Viktor Poulsen <viktor@maigaard.io>
2016-10-03 08:43:22 +0200

    Added comments to dicecup

--- src/DiceGame/DiceCup.java
+++ src/DiceGame/DiceCup.java
@@ -3,13 +3,22 @@ package DiceGame;
 import java.lang.reflect.Array;
 
 /**
- *
+ * Dicecup class
  */
 public class DiceCup {
     private Dice[] dices = new Dice[5];
+
+    /**
+     * Default constructor creates cup with 2 dices
+     */
     public DiceCup(){
         this(2);
     }
+
+    /**
+     * Alternate constructor allow dicecup to be created with more/less than two dices (maximum of 5)
+     * @param diceCount
+     */
     public DiceCup(int diceCount){
         if(diceCount>5){
             System.out.print("The cup can only hold up to 5 dices.");
@@ -21,6 +30,10 @@ public class DiceCup {
         }
     }
 
+    /**
+     * Rolls all dices in cup
+     * TODO: Fix for-loop to be indexed instead of foreach.
+     */
     public void throwDices(){
         for (Dice dice:dices) {
             dice.Roll();
